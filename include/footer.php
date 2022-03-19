3dbe7d9953718d8c73b6da5837e60aa8fc898d6e
Maksim Majmari <maksimmajmari12@gmail.com>
2022-03-19 23:33:03 +0100

    designing the home page

---
+++ include/footer.php
@@ -0,0 +1,15 @@
+
+<footer class="footer">
+      <div class="container">
+       <p class="text-muted">Copyright &copy; <a href="https://api.whatsapp.com/send?phone=+355+68+534+4136">Maxi</a> 2022</p>
+<!--        <span class="text-muted">Place sticky footer content here.</span>-->
+      </div>
+</footer>        
+    </div>
+    <!-- /.container -->
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,60 @@
+<?php include "include/header.php"?>
+
+<?php include "include/navigation.php"?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-18">
+            <hr>
+            <div>
+                <h1>Hello here are some photo of exams that the other student allready have share. Please <a href="#">click here</a> for sharing an other photo of any exams.</h1>
+            </div>
+            
+            
+            
+            
+            <a href="#">
+                <div class="cards">
+                    <h1 class="title"> Bachelor Matematike </h1>
+                </div>
+            </a>
+            <a href="#">
+                <div class="cards">
+                    <h1 class="title"> Bachelor </h1>
+                </div>
+            </a>
+            <a href="#">
+                <div class="cards">
+                    <h1 class="title"> Bachelor </h1>
+                </div>
+            </a>
+            <a href="#">
+                <div class="cards">
+                    <h1 class="title"> Bachelor </h1>
+                </div>
+            </a>
+            <a href="#">
+                <div class="cards">
+                    <h1 class="title"> Bachelor </h1>
+                </div>
+            </a>
+            <a href="#">
+                <div class="cards">
+                    <h1 class="title"> Bachelor </h1>
+                </div>
+            </a>
+            <a href="#">
+                <div class="cards">
+                    <h1 class="title"> Bachelor </h1>
+                </div>
+            </a>
+            </a>
+
+        </div>
+
+    </div>
+</div>
+
+</div>
+
+<?php include "include/footer.php"?>
